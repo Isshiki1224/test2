780abdcde00d9406592fbe3d4dce7ed0d09a9c1f
leon <leonfrer@gmail.com>
2020-08-26 11:00:20 +0800

    upload

---
+++ src/main/java/com/xm/commerce/system/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.xm.commerce.system.controller;
+
+import com.xm.commerce.security.util.CurrentUserUtils;
+import com.xm.commerce.system.model.response.ResponseCode;
+import com.xm.commerce.system.model.response.ResponseData;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+	private final CurrentUserUtils currentUserUtils;
+
+	public UserController(CurrentUserUtils currentUserUtils) {
+		this.currentUserUtils = currentUserUtils;
+	}
+
+	@GetMapping
+	public ResponseData getCurrentUser() {
+		String currentUsername = currentUserUtils.getCurrentUsername();
+		return new ResponseData(ResponseCode.SUCCESS, currentUsername);
+	}
+}
